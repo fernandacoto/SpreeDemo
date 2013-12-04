1ec9cc921b4ab71ac9bf4ce740d9027d89cd6209
Fernanda <fermarioro@gmail.com>
2013-12-04 10:47:51 -0600

    Spree demo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ThrillJockey::Application.config.secret_key_base = 'c979d1a724a5ae15dc2574fa1960e32d61a38750af0443254fb9ce984913429fd0c16e380fa4c2ce759461b68bf2020b90ebf1e2c082de7303bffb75d9f65412'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ThrillJockey::Application.config.session_store :cookie_store, key: '_thrill_jockey_session'
